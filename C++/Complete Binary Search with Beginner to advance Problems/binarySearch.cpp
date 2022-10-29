b50d975ced9ed378d0851ac6dc48de9164e8bcf1
Akshat Bansal <95624168+Akshatkba@users.noreply.github.com>
2022-10-29 12:28:10 +0530

    Recursive solution for binary search
    Added a recursive solution for binary search instead of an iterative one.

--- C++/Complete Binary Search with Beginner to advance Problems/binarySearch.cpp
+++ C++/Complete Binary Search with Beginner to advance Problems/binarySearch.cpp
@@ -1,49 +1,29 @@
 #include<iostream>
 using namespace std;
-
-int binarySearch(int arr[], int size, int key) {
-
-    int start = 0;
-    int end = size-1;
-
+bool search(int arr[],int start,int end,int element){
     int mid = start + (end-start)/2;
 
-    while(start <= end) {
+    if(arr[mid]==element)
+        return true;
+    if(start==end)
+        return 0;
 
-        if(arr[mid] == key) {
-            return mid;
-        }
-
-        //go to right wala part
-        if(key > arr[mid]) {
-            start = mid + 1;
-        }
-        else{ //key < arr[mid]
-            end = mid - 1;
-        }
-
-        mid = start + (end-start)/2;
-    }
-    
-    return -1;
+    if(arr[mid]>element)
+        return search(arr,start,mid-1,element);
+    else
+        return search(arr,mid+1,end,element);
 }
-
-
-int main() { 
-
+int main()
+{
     int even[6] = {2,4,6,8,12,18};
     int odd[5] = {3, 8, 11, 14, 16};
 
-    int evenIndex = binarySearch(even, 6, 6);
-
-    cout << " Index of 6 is " << evenIndex << endl;
-
-    int oddIndex = binarySearch(odd, 5, 14);
-
-    cout << " Index of 14 is " << oddIndex << endl;
-
-
-    return 0;
+    if(search(even,0,6,0))
+        cout<<"Found!"<<endl;
+    else
+        cout<<"Not Found!"<<endl;
+        
+return 0;
 }
 
 int findPeak(int arr[], int n) {
